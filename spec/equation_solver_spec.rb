479fa299b1ac30a878ca3b4df5950689e93577cc
denio-rus <denio@yandex.ru>
2019-09-12 18:15:37 +0700

    intend

--- spec/equation_solver_spec.rb
+++ spec/equation_solver_spec.rb
@@ -13,39 +13,39 @@ RSpec.describe EquationSolver do
   let(:lin_equation_missed_a_param) { {  'b_param' => -12, 'type' => 'linear' } }
 
   it 'returns an array of roots if Discriminant is positive' do
- 	expect(EquationSolver.new(quad_equation_pos_discr).solve).to eq([6.0, 2.0])
+ 	  expect(EquationSolver.new(quad_equation_pos_discr).solve).to eq([6.0, 2.0])
   end
 
   it "returns string 'No roots' if Discriminant is negative" do
- 	expect(EquationSolver.new(quad_equation_neg_discr).solve).to eq('No roots')
+ 	  expect(EquationSolver.new(quad_equation_neg_discr).solve).to eq('No roots')
   end
 
   it "returns number if Discriminant is equal to zero" do
- 	expect(EquationSolver.new(quad_equation_zero_discr).solve).to eq(3.0)
+ 	  expect(EquationSolver.new(quad_equation_zero_discr).solve).to eq(3.0)
   end
 
   it "returns root if given two number arguments" do
- 	expect(EquationSolver.new(lin_equation).solve).to eq(6)
+ 	  expect(EquationSolver.new(lin_equation).solve).to eq(6)
   end
 
   it 'raises exeption if wrong param given' do
-   	expect { EquationSolver.new(quad_equation_invalid_param) }.to raise_error(ArgumentError)
+    expect { EquationSolver.new(quad_equation_invalid_param) }.to raise_error(ArgumentError)
   end
 
   it 'raises exeption if wrong param given' do
-   	expect { EquationSolver.new(lin_equation_invalid) }.to raise_error(ArgumentError)
+    expect { EquationSolver.new(lin_equation_invalid) }.to raise_error(ArgumentError)
   end
 
   it 'raises exeption if wrong number of params given to quadratic' do
-   	expect { EquationSolver.new(quad_equation_missed_param) }.to raise_error(ArgumentError)
+    expect { EquationSolver.new(quad_equation_missed_param) }.to raise_error(ArgumentError)
   end
 
   it 'raises exeption if wrong number of params given to linear' do
-   	expect { EquationSolver.new(lin_equation_unnecessary_param) }.to raise_error(ArgumentError)
+    expect { EquationSolver.new(lin_equation_unnecessary_param) }.to raise_error(ArgumentError)
   end
 
   it 'raises exeption if a param is not given' do
-   	expect { EquationSolver.new(quad_equation_missed_a_param) }.to raise_error(ArgumentError)
-   	expect { EquationSolver.new(lin_equation_missed_a_param) }.to raise_error(ArgumentError)
+    expect { EquationSolver.new(quad_equation_missed_a_param) }.to raise_error(ArgumentError)
+    expect { EquationSolver.new(lin_equation_missed_a_param) }.to raise_error(ArgumentError)
   end
 end
